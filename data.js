2c54cb213e23b1e523a40952f5bd55425954aa8c
Bonspiel2 <cole_adams@shaw.ca>
2016-04-09 16:58:57 -0600

    Added places

---
+++ data.js
@@ -0,0 +1,11 @@
+window.alert(5);
+/*txtFile.open("GET", "https://raw.githubusercontent.com/Cobresun/Cobresun.github.io/master/assets/CityAmenitiesText.txt", true);
+txtFile.onreadystatechange = function(){
+	if (txtFile.readyState == 4) {
+		if(txtFile.status == 200) {
+			allText = txtFile.responseText;
+			document.getElementById('business');
+		}
+	}
+}
+txtFile.send(null);
