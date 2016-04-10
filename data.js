c96ae9269159f35d9cbeb0cad1a622ef7383be49
Bonspiel2 <cole_adams@shaw.ca>
2016-04-09 19:50:53 -0600

    worked on fb integration

--- data.js
+++ data.js
@@ -1,11 +1,31 @@
-window.alert(5);
-/*txtFile.open("GET", "https://raw.githubusercontent.com/Cobresun/Cobresun.github.io/master/assets/CityAmenitiesText.txt", true);
-txtFile.onreadystatechange = function(){
-	if (txtFile.readyState == 4) {
-		if(txtFile.status == 200) {
-			allText = txtFile.responseText;
-			document.getElementById('business');
-		}
-	}
+window.alert(1);
+$(document).ready(function() {
+  window.alert(2);
+    $.ajax({
+        type: "GET",
+        url: "http://raw.githubusercontent.com/Cobresun/Cobresun.github.io/master/assets/CityAmenitiesText.txt",
+        dataType: "text",
+        success: function(data) {window.alert(3);processData(data);}
+     });
+});
+
+function processData(allText) {
+  window.alert(4);
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(headers[j]+":"+data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+    window.alert(5);
+    // alert(lines);
 }
-txtFile.send(null);
